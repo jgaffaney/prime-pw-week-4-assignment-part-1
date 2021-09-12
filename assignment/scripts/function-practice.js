89da689ad879e40739efc3ae6406c9cc2dc7fb51
John Gaffaney <john.gaffaney@icloud.com>
2021-09-12 14:18:44 -0500

    made minor adjustments.  all complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -27,12 +27,14 @@ function addNumbers( firstNumber, secondNumber ) {
   return firstNumber + secondNumber;
 }
 console.log('TEST - Should say 15', addNumbers(9,6));
+console.log('TEST - Should say 3', addNumbers(9, -6));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( num0, num1, num2 ) {
 return num0 * num1 * num2;
 }
 console.log('TEST - Shoud say 315', multiplyThree(5,7,9));
+console.log('TEST - Should say -315', multiplyThree(5, -7, 9));
 
 
 // 5. Function that will return true if a number is positive,
@@ -61,6 +63,8 @@ return array[lastItemindex];
 
 names = ['Peter', 'James', 'Steven', 'Robert']
 console.log('TEST - should say Robert', getLast(names));
+numberArray = [4, 7, 2, 8, 10]
+console.log('TEST - should say 10', getLast(numberArray));
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
@@ -81,12 +85,12 @@ function find( value, array ){
   return answer;
 }// end find
 
-console.log('TEST - should return true', find(44, numbers));
-console.log('TEST - Should return false', find(56, numbers));
-console.log('TEST - Should return false', find(true, numbers));
-console.log('TEST - Should return true', find(77, numbers));
-console.log('TEST - Should return true', find('Steven', names));
-console.log('TEST - Should return false', find(34, names));
+console.log('TEST find() - should return true', find(44, numbers));
+console.log('TEST find() - Should return false', find(56, numbers));
+console.log('TEST find() - Should return false', find(true, numbers));
+console.log('TEST find() - Should return true', find(77, numbers));
+console.log('TEST find() - Should return true', find('Steven', names));
+console.log('TEST find() - Should return false', find(34, names));
 
 
 // ----------------------
@@ -95,14 +99,13 @@ console.log('TEST - Should return false', find(34, names));
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  let ans;
+  //let ans;
   if(letter === string[0]) {
-    ans = true;
+    return true;
   } // end if
     else{
-      ans = false;
+      return false;
     }//end else
-  return ans;
 }//end function
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
@@ -131,7 +134,9 @@ function listPositive(array) {
   return positiveArray;
 }//end function
 arrayWithNegative = [3, 6, 9, 2, -4, -6, 45];
-console.log('TEST - should say return array with 5 items', listPositive(arrayWithNegative));
+console.log('TEST - should eturn array with 5 items', listPositive(arrayWithNegative));
+console.log('TEST - should return array with 4, 7, 2, 8, 10', listPositive(numberArray));
+
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
